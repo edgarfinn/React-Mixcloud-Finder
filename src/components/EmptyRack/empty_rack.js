a865c3f82c03f36e746df6489ff71155ba5400f1
Edgar Dewsbery <edewsbery@gmail.com>
2017-12-05 12:30:30 +0000

    #2 darken empty rack background

--- src/components/EmptyRack/empty_rack.js
+++ src/components/EmptyRack/empty_rack.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './empty_rack.css';
+import EmptySpacer from '../EmptySpacer/empty_spacer';
 
 const EmptyRack = () => {
   let rackContainer = [];
   while(rackContainer.length < 20) {
+    const lastInRack = rackContainer.length > 18;
+    console.log();
     const EmptyUnit =
     <li key={rackContainer.length} className="empty-unit">
-        <div className="empty-spacer">
-          <div className="empty-ridge empty-ridge-left"></div>
-          <div className="empty-ridge empty-ridge-right"></div>
-        </div>
+      {!lastInRack &&
+        <EmptySpacer />
+      }
     </li>;
     rackContainer.push(EmptyUnit);
   }
